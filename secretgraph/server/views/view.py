6467a5418a4a0167ce85f0531ba2b49843ab5ac7
alex <devkral@web.de>
2020-05-02 22:43:17 +0200

    progress

--- secretgraph/server/views/view.py
+++ secretgraph/server/views/view.py
@@ -1,19 +1,12 @@
-import json
-
 from django.views.generic import View
 from django.http import StreamingHttpResponse, Http404
 
-from ..utils.misc import FakeList
 from ..utils.encryption import iter_decrypt_contents
 from ..actions.view import fetch_contents
 
 
-encoder = json.JsonEncoder()
-
-
 class DocumentsView(View):
     model = None
-    decrypt = False
 
     def get(self, request, *args, **kwargs):
         authset = set(request.headers.get("Authorization", "").replace(
@@ -22,34 +15,17 @@ class DocumentsView(View):
         result = fetch_contents(
             request, query=kwargs.get("id"), authset=authset
         )
-        if self.decrypt:
-            def gen():
-                seperator = b""
-                for document in iter_decrypt_contents(
-                    result["objects"], authset
-                ):
-                    yield seperator
-                    for chunk in document:
-                        yield chunk.replace(b"\0", b"\\0")
-                    seperator = b"\0"
-        else:
-            def gen():
-                seperator = b""
-                for content in result["objects"]:
-                    yield seperator
-                    if kwargs.get("id"):
-                        yield b"nonce\0"
-                        yield b"keys\0"
-                    with content.value.open("rb") as fileob:
-                        chunk = fileob.read(512)
-                        while chunk:
-                            yield chunk.replace(b"\0", b"\\0")
-                            chunk = fileob.read(512)
-                    seperator = b"\0"
 
-        response = StreamingHttpResponse(
-            encoder.iterencode(gen())
-        )
+        def gen():
+            seperator = b""
+            for document in iter_decrypt_contents(
+                result["objects"], authset
+            ):
+                yield seperator
+                for chunk in document:
+                    yield chunk.replace(b"\0", b"\\0")
+                seperator = b"\0"
+        if not result["objects"]:
+            raise Http404()
 
-        # check permission, redirect or directly offer, add nonces as header
-        return response
+        return StreamingHttpResponse(gen())
