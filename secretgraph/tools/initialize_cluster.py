a8c1fb5e3c90ec4e486ce717c8afcaeff1d67fc7
alex <devkral@web.de>
2020-10-02 18:55:17 +0200

    progress

--- secretgraph/tools/initialize_cluster.py
+++ secretgraph/tools/initialize_cluster.py
@@ -1,12 +1,18 @@
 
 import argparse
 
+import requests
+
 parser = argparse.ArgumentParser()
 parser.add_argument("url")
 
 
 def main(argv=None):
     argv = parser.parse_args(argv)
+    session = requests.Session()
+    result = session.post(
+        argv.url
+    )
 
 
 if __name__ == "__main__":
