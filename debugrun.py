d2600520a78f0c79f23a5b5299c556110ab4cec2
alex <devkral@web.de>
2021-06-08 20:39:00 +0200

    fix debugrun, update deps

--- debugrun.py
+++ debugrun.py
@@ -1,7 +1,14 @@
 #! /usr/bin/env python3
 import subprocess
-from manage import main
+
 
 if __name__ == "__main__":
-    with subprocess.Popen(["npm", "run", "serve:dev"]):
-        main()
+    with subprocess.Popen(["npm", "run", "serve:dev"]) as npm:
+        with subprocess.Popen(
+            ["python", "./manage.py", "runserver_plus"]
+        ) as django:
+            while npm.poll() is None and django.poll() is None:
+                try:
+                    django.communicate(timeout=50)
+                except Exception:
+                    pass
