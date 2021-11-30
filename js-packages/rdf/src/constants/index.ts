4ecad042b72a63206b7b727e9859e86ef4aab318
alex <devkral@web.de>
2021-11-30 23:04:38 +0100

    update deps, replace rdflib with n3

--- js-packages/rdf/src/constants/index.ts
+++ js-packages/rdf/src/constants/index.ts
@@ -1,12 +1,7 @@
-/**import { Namespace } from 'rdflib'
-
-export const RDF = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-export const RDFS = Namespace('http://www.w3.org/2000/01/rdf-schema#')
-export const XSD = Namespace('http://www.w3.org/2001/XMLSchema#')
-export const SECRETGRAPH = Namespace(
+export const RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+export const RDFS = 'http://www.w3.org/2000/01/rdf-schema#'
+export const XSD = 'http://www.w3.org/2001/XMLSchema#'
+export const SECRETGRAPH =
     'https://secretgraph.net/static/schemes/secretgraph/secretgraph#'
-)
-export const CLUSTER = Namespace(
+export const CLUSTER =
     'https://secretgraph.net/static/schemes/secretgraph/cluster#'
-)
-*/
