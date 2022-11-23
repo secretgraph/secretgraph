66eb35502d3edc76f76050da555b178114967d35
alex <devkral@web.de>
2022-11-23 23:14:11 +0100

    fix service worker

--- assets/js/serviceworker.ts
+++ assets/js/serviceworker.ts
@@ -13,9 +13,9 @@ async function grab_assets(
     const resp = await fetch(manifest_path, { cache })
     if (resp.ok) {
         const assets: string[] = Object.values(await resp.json())
-        for (const ext of externals) {
-            if (!assets.includes(ext)) {
-                assets.push(ext)
+        for (const external of externals) {
+            if (!assets.includes(external)) {
+                assets.push(external)
             }
         }
         return assets
@@ -24,7 +24,7 @@ async function grab_assets(
 }
 async function installSW() {
     const [precachedAssets, cache] = await Promise.all([
-        grab_assets(),
+        grab_assets('no-cache'),
         caches.open(cacheName),
     ])
     if (dev) {
@@ -46,40 +46,38 @@ self.addEventListener('install', (event: ExtendableEvent) => {
 })
 
 async function interceptFetch(event: FetchEvent): Promise<Response> {
-    let response = undefined
+    let response: undefined | Response = undefined
     if (event.request.method == 'GET' || event.request.method == 'HEAD') {
-        if (
-            event.request.url.includes('/static/') ||
-            event.request.url.endsWith('/client/')
-        ) {
-            const cache = await caches.open(cacheName)
-            response = await cache.match(event.request.url, {
-                ignoreSearch: true,
-            })
+        const url = new URL(event.request.url)
+        const cache = await caches.open(cacheName)
+        // serving cached client here can lead to big problems
+        if (url.pathname.startsWith('/static/')) {
+            response = await cache.match(event.request.url)
             if (response) {
                 return response
             }
-            if (
-                event.request.url.endsWith('/client/') ||
-                event.request.url.endsWith('/favicon.svg')
-            ) {
-                await cache.add(event.request)
-                response = await cache.match(event.request, {
-                    ignoreSearch: true,
-                })
-                if (response) {
-                    return response
-                } else {
-                    console.debug(
-                        'could not intercept 2nd try: ' + event.request.url
-                    )
-                }
-            }
-        } else {
-            console.debug('could not intercept: ' + event.request.url)
         }
+        let cacheResponse =
+            url.pathname.startsWith('/static/') ||
+            url.pathname.search(/\/client\/?$/) >= 0
+        try {
+            response = await fetch(event.request)
+        } catch (error) {
+            cacheResponse = false
+            // serving client from cache when no network is no problem
+            response = await cache.match(event.request.url)
+            if (!response) {
+                throw error
+            }
+        }
+        // cache and static are cached
+        if (cacheResponse) {
+            await cache.put(event.request, response.clone())
+        }
+
+        return response
     }
-    return fetch(event.request)
+    return await fetch(event.request)
 }
 
 self.addEventListener('fetch', (event: FetchEvent) => {
