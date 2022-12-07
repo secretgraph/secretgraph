67204c86c391f146df3e255ae149dd16f2467734
alex <devkral@web.de>
2022-12-07 02:05:13 +0100

    improve orphans command

--- secretgraph/server/management/commands/sg_orphans.py
+++ secretgraph/server/management/commands/sg_orphans.py
@@ -14,11 +14,6 @@ class Command(BaseCommand):
             "--delete", action="store_true", help="Delete Orphans"
         )
         parser.add_argument(
-            "--inactive",
-            action="store_true",
-            help="List (and delete) inactive",
-        )
-        parser.add_argument(
             "-w",
             "--what",
             choices=["Cluster", "Net"],
@@ -27,7 +22,7 @@ class Command(BaseCommand):
         )
 
     def handle(self, **options):
-        inactive = timezone.now() - timedelta(weeks=52)
+        unused = timezone.now() - timedelta(weeks=52)
         if "Cluster" in options["what"]:
             annotated_clusters = Cluster.objects.annotate(
                 last_action_used=Subquery(
@@ -36,62 +31,90 @@ class Command(BaseCommand):
                     .values("used")[:1]
                 ),
             ).exclude(name="@system")
-            orphan_clusters = annotated_clusters.filter(contents__isnull=True)
-            if orphan_clusters:
-                print("Orphan Clusters:")
-                for c in orphan_clusters:
+            q_orphan = Q(contents__isnull=True)
+            q_unused = (
+                Q(last_action_used__isnull=True) & Q(updated__lt=unused)
+            ) | Q(last_action_used__lt=unused)
+            orphan_and_unused_clusters = annotated_clusters.filter(
+                q_orphan, q_unused
+            )
+            orphan_clusters = annotated_clusters.filter(q_orphan).exclude(
+                id__in=Subquery(orphan_and_unused_clusters.values("id"))
+            )
+            if orphan_and_unused_clusters:
+                print(
+                    "Orphan and unused Clusters (no contents, "
+                    "no interaction for some time):"
+                )
+                for c in orphan_and_unused_clusters:
                     print(
-                        "  {!r} inactive since {}".format(
+                        "  {!r} unused since {}".format(
                             c, c.last_action_used or c.updated
                         )
                     )
-                if options["delete"]:
-                    orphan_clusters.delete()
-            if options["inactive"]:
-                inactive_clusters = annotated_clusters.exclude(
-                    id__in=Subquery(orphan_clusters.values("id"))
-                ).filter(
-                    (
-                        Q(last_action_used__isnull=True)
-                        & Q(created__lt=inactive)
+                    if options["delete"]:
+                        orphan_and_unused_clusters.delete()
+            if orphan_clusters:
+                print(
+                    "Orphan Clusters (no contents{}):".format(
+                        ", not deleted" if options["delete"] else ""
                     )
-                    | Q(last_action_used__lt=inactive)
                 )
-
-                print("Inactive Clusters:")
-                for c in inactive_clusters:
+                for c in orphan_clusters:
                     print(
-                        "  {!r} inactive since {}".format(
+                        "  {!r} unused since {}".format(
+                            c, c.last_action_used or c.updated
+                        )
+                    )
+            unused_clusters = annotated_clusters.filter(q_unused).exclude(
+                id__in=Subquery(orphan_and_unused_clusters.values("id"))
+            )
+            if unused_clusters:
+                print(
+                    "Unused Clusters{}:".format(
+                        " (not deleted)" if options["delete"] else ""
+                    )
+                )
+                for c in unused_clusters:
+                    print(
+                        "  {!r} unused since {}".format(
                             c, c.last_action_used or c.updated
                         )
                     )
 
         if "Net" in options["what"]:
-            q = Q()
-            if options["inactive"]:
-                q = Q(last_action_used__isnull=False) & Q(
-                    last_action_used__gte=inactive
-                ) | Q(last_used_gt=inactive)
-            vivid_clusters = Cluster.objects.filter(contents__isnull=False)
-            orphan_nets = (
-                Net.objects.annotate(
-                    last_action_used=Subquery(
-                        Action.objects.filter(cluster__net_id=OuterRef("id"))
-                        .order_by("-used")
-                        .values("used")[:1]
-                    )
-                )
-                .exclude(id=0)
-                .filter(
-                    ~Exists(vivid_clusters.filter(net_id=OuterRef("id"))), q
-                )
+            # negative query, what scores for inactivity
+            q = (
+                Q(last_action_used__isnull=False)
+                & Q(last_action_used__lt=unused)
+            ) | (Q(last_action_used__isnull=True) & Q(last_used__lt=unused))
+            # can contain contents with other nets
+            # CLARIFY: but they are still updated via actions
+            # MAYBE: move last_Action_used check behind a switch
+            vivid_clusters = Cluster.objects.filter(
+                Q(contents__updated__gte=unused)
             )
-            if orphan_nets:
-                print("Orphan Nets:")
-                for c in orphan_nets:
+            annotated_nets = Net.objects.annotate(
+                last_action_used=Subquery(
+                    Action.objects.filter(cluster__net_id=OuterRef("id"))
+                    .order_by("-used")
+                    .values("used")[:1]
+                ),
+                # last_used contains latest content and cluster update
+            ).exclude(id=0)
+            orphan_and_unused_nets = annotated_nets.filter(
+                ~Exists(vivid_clusters.filter(net_id=OuterRef("id"))),
+                q,
+            )
+            if orphan_and_unused_nets:
+                print(
+                    "Orphan and unused Nets (no contents and "
+                    "no interaction for some time):"
+                )
+                for c in orphan_and_unused_nets:
                     latest = c.last_used
                     if c.last_action_used and c.last_action_used > latest:
                         latest = c.last_action_used
-                    print("  {!r} inactive since {}".format(c, latest))
+                    print("  {!r} unused since {}".format(c, latest))
                 if options["delete"]:
-                    orphan_nets.delete()
+                    orphan_and_unused_nets.delete()
