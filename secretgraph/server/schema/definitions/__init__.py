2a7972f45e64d91861054a09152703a87a05be55
alex <devkral@web.de>
2023-01-27 05:53:30 +0100

    remove FlexidType

--- secretgraph/server/schema/definitions/__init__.py
+++ secretgraph/server/schema/definitions/__init__.py
@@ -1,8 +1,5 @@
-from typing import Union
 from .clusters import *  # noqa F403
 from .contents import *  # noqa F403
 from .environment import *  # noqa F403
 from .references import *  # noqa F403
 from .misc import *  # noqa F403
-
-FlexidType = Union[ClusterNode, ContentNode]  # noqa F405
