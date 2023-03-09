7a96321fff27868cf643df43aebc58e3a645f02b
alex <devkral@web.de>
2023-03-09 05:44:09 +0100

    fix Datetime picker

--- js-packages/client/src/components/formik/FormikDateTimePicker.tsx
+++ js-packages/client/src/components/formik/FormikDateTimePicker.tsx
@@ -1,46 +1,38 @@
 import TextField from '@mui/material/TextField'
-import { DateTimePicker, DateTimePickerProps } from '@mui/x-date-pickers/DateTimePicker'
+import {
+    DateTimePicker,
+    DateTimePickerProps,
+} from '@mui/x-date-pickers/DateTimePicker'
 import { OptionalAttributes } from '@secretgraph/misc/typing'
 import { FieldProps } from 'formik'
 import * as React from 'react'
 
 export type FormikDateTimePickerProps<
-    V extends string | string[] = string,
+    V extends string = string,
     FormValues = any
-> = OptionalAttributes<
-    Omit<
-        DateTimePickerProps<string, Date>,
-        keyof FieldProps<V, FormValues> | 'defaultValue' | 'onChange'
-    >,
-    'renderInput'
+> = Omit<
+    DateTimePickerProps<string>,
+    keyof FieldProps<V, FormValues> | 'defaultValue' | 'onChange'
 > &
     FieldProps<V, FormValues>
 
 export default function FormikDateTimePicker<
-    V extends string | string[] = string,
+    V extends string = string,
     FormValues = any
 >({
     field,
     form,
     meta: metaIntern,
-    renderInput,
     ampm,
-    inputFormat,
-    mask,
     ...params
 }: FormikDateTimePickerProps<V, FormValues>) {
     return (
         <DateTimePicker
-            renderInput={
-                renderInput ? renderInput : (props) => <TextField {...props} />
-            }
-            inputFormat={inputFormat ? inputFormat : 'yyyy-MM-dd hh:mm'}
-            mask={mask ? mask : '____-__-__ __:__'}
             // TODO: use until code is fixed
             ampm={ampm !== undefined ? ampm : false}
             {...field}
             {...params}
-            onChange={(val) => {
+            onChange={(val, context) => {
                 form.setFieldValue(field.name, val)
                 form.setFieldTouched(field.name, true)
             }}
