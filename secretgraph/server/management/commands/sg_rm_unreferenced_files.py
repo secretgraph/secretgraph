5f43affb280f3a424ae612e6ebf784c626ebff10
alex <devkral@web.de>
2023-11-29 17:04:27 +0100

    add command for pruning media folder

---
+++ secretgraph/server/management/commands/sg_rm_unreferenced_files.py
@@ -0,0 +1,44 @@
+import posixpath
+
+from django.core.files.storage import default_storage
+from django.core.management.base import BaseCommand
+
+from ...models import Content
+
+
+class Command(BaseCommand):
+    help = "Remove unreferenced files in media"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--dry-run", action="store_true")
+
+    def handle_directory(self, keep_set, dry_run, directory):
+        directories, files = default_storage.listdir(directory)
+        count = len(files) + len(directories)
+        for file in files:
+            full_file = posixpath.join(directory, file)
+            if full_file not in keep_set:
+                if dry_run:
+                    print("would remove file", full_file)
+                else:
+                    default_storage.remove(full_file)
+                count -= 1
+
+        for sub_directory in directories:
+            if self.handle_directory(
+                keep_set, dry_run, posixpath.join(directory, sub_directory)
+            ):
+                count -= 1
+        if count == 0:
+            if dry_run:
+                print("would remove directory", directory)
+            else:
+                default_storage.remove(directory)
+            return True
+        return False
+
+    def handle(self, dry_run, **options):
+        keep_set = set(Content.objects.values_list("file", flat=True))
+        print("keep set", keep_set)
+        if default_storage.exists(""):
+            self.handle_directory(keep_set, dry_run, "")
